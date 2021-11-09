b2e3bd594587bb117d6049fb0fd1953d165f073f
cabart <abartc@student.ethz.ch>
2021-11-09 15:39:03 +0100

    changed network structure to n senders + switch + receiver

--- profile.py
+++ profile.py
@@ -13,19 +13,52 @@ import geni.rspec.pg as pg
 # Create a portal context.
 pc = portal.Context()
 
+# Add parameters
+pc.defineParameter("sender","Number of sender nodes", portal.ParameterType.INTEGER, 2)
+
+# Retrieve user input from instantiation
+params = pc.bindParameters()
+
 # Create a Request object to start building the RSpec.
 request = pc.makeRequestRSpec()
  
-# Add two VMs
-node1 = request.XenVM("node1")
-node2 = request.XenVM("node2")
+if params.sender < 1 or params.sender > 10:
+    pc.reportError(portal.ParameterError("Number of senders should be between 1 and 10"))
+
+# Add switch
+switch = request.Switch("sw")
+
+# Add receiver VM
+nodeRcv = request.XenVM("rcv")
+rcv_iface = nodeRcv.addInterface()
+
+# Create link between switch and receiver node
+swiface = switch.addInterface()
+link = request.L1Link("rcvlink")
+link.addInterface(rcv_iface)
+link.addInterface(swiface)
+
+# Add sender VMs
+for i in range(params.sender):
+    # create node + interface + address
+    node = request.XenVM("sender" + str(i))
+    iface = node.addInterface()
+    iface.addAddress(pg.IPv4Address("192.168.1." + str(i),"255.255.255.0"))
+    # Add startup script
+    node.addService(pg.Execute(shell="sh", command="/local/repository/startup.sh"))
+    # Add interface at switch
+    swiface = switch.addInterface()
+    # Add link to switch
+    link = request.L1Link("link" + str(i))
+    link.addInterface(iface)
+    link.addInterface(swiface)
+
+
+
+
 
 # Add link between nodes
 link1 = request.Link(members = [node1, node2])
-
-# Install and execute a script that is contained in the repository.
-node1.addService(pg.Execute(shell="sh", command="/local/repository/startup.sh"))
-node2.addService(pg.Execute(shell="sh", command="/local/repository/startup.sh"))
 
 # Print the RSpec to the enclosing page.
 pc.printRequestRSpec(request)

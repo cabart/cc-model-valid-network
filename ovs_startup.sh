e5257b5f58f796fb5b5edb10799595abe81d13b1
cabart <abartc@student.ethz.ch>
2021-12-16 13:38:26 +0100

    fixed receiver not in switch bridge

--- ovs_startup.sh
+++ ovs_startup.sh
@@ -8,7 +8,10 @@ do
 		n) sender=${OPTARG};;
 	esac
 done
+
 echo "number of senders $sender" >> /tmp/startup.txt
+((sender++))
+echo "number of total nodes $sender" >> /tmp/startup.txt
 
 sudo apt update
 sudo apt install -y openvswitch-switch
@@ -28,4 +31,4 @@ done
 
 sudo ovs-vsctl set-fail-mode br0 standalone
 
-sudo ovs-vsctl show >> /tmp/startup.txt
+sudo ovs-vsctl show > /tmp/switch_bridge.txt
